26bab701776dc0fdef7799b0209a6d07a90cd41e
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-01-27 11:10:38 -0600

    Updated server.py, home.html, and completed task 2

--- server.py
+++ server.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("home.html", color_1 = "black", color_2 = "red", num = 8)
+def checkboard_1():
+    return render_template("home.html", color_1 = "black", color_2 = "red", row = 8, col = 8)
+
+@app.route("/<int:num>")
+def checkboard_2(num):
+    return render_template("home.html", color_1 = "black", color_2 = "red", row = num, col = 8)
 
 if __name__ == "__main__":
     app.run(debug=True)
