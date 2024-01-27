489d4321aab8313b8323eadbe4667a36c7a2e8c3
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2024-01-27 11:28:42 -0600

    Updated server.py, completed bonus task

--- server.py
+++ server.py
@@ -3,16 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def checkboard_1():
+def checkerboard_1():
     return render_template("home.html", color_1 = "black", color_2 = "red", row = 8, col = 8)
 
 @app.route("/<int:num>")
-def checkboard_2(num):
+def checkerboard_2(num):
     return render_template("home.html", color_1 = "black", color_2 = "red", row = num, col = 8)
 
 @app.route("/<int:x>/<int:y>")
-def checkboard_3(x,y):
+def checkerboard_3(x,y):
     return render_template("home.html", color_1 = "black", color_2 = "red", row = x, col = y)
+
+@app.route("/<string:color1>/<int:x>/<string:color2>/<int:y>")
+def checkerboard_4(color1,x,color2,y):
+    return render_template("home.html", color_1=color1, row=x, color_2=color2, col=y)
 
 if __name__ == "__main__":
     app.run(debug=True)
